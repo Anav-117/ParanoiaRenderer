f48960f47178aa34573f081218093720179e2b8c
Anav-117 <dr4g04@gmail.com>
2025-03-19 16:54:02 -0400

    initial implementation

--- Defines.cuh
+++ Defines.cuh
@@ -3,10 +3,11 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#include "cuda_runtime.h"
+#include <cuda_runtime.h>
 
-#define HOD		inline __host__ __device__
-#define HO		inline __host__
-#define DEV		inline __device__
+#define HOD		__host__ __device__
+#define HO		__host__
+#define DEV		__device__
+#define UINT	unsigned int
 
 #endif
--- Source.cu
+++ Source.cu
@@ -1,5 +1,5 @@
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 #include "Defines.cuh"
 #include "Utils.cuh"
 #include "Vec3.cuh"
@@ -11,36 +11,84 @@
 #include <curand.h>
 #include <curand_kernel.h>
 #include <vector>
+#include <string>
 
-__global__ void simulate(float* PersonalityField) {
+#define NUM_SIMULATIONS 100
+
+HOD struct Agent {
+
+    float Kp = 1.0;
+    float Reach = 1.0;
+    float Ks = 1.0;
+    float personality = 0.0;
+
+    float PersonalityPressure = 0.0;
+    float InfluenceNumber = 0.0;
+
+};
+
+__global__ void ApplyPersonalityPressure(Agent* AgentField, int FieldHeight, int FieldWidth) {
 
     int j = blockIdx.x * blockDim.x + threadIdx.x;
+    vec3 AgentPos = vec3(blockIdx.x, threadIdx.x, 0);
 
-    float personality = PersonalityField[j];
+    for (UINT y = 0; y < FieldHeight; y++) {
+        for (UINT x = 0; x < FieldWidth; x++) {
+            int index = y * blockDim.x + x;
+            if (index == j) {
+                continue;
+            }
 
-    PersonalityField[j] = 1.0 - personality;
+            vec3 pos = vec3(y, x, 0);
+
+            float wieghtedDist = (AgentPos - pos).length() / AgentField[j].Reach;
+
+            float personalityPressure = (AgentField[index].Kp * (AgentField[j].personality - AgentField[index].personality)) / wieghtedDist;
+
+            AgentField[index].PersonalityPressure += personalityPressure;
+            AgentField[index].InfluenceNumber += 1.0;
+            //atomicAdd((float*)(AgentField + index * sizeof(Agent) + 4 * sizeof(float)), personalityPressure);
+            //atomicAdd((float*)(AgentField + index * sizeof(Agent) + 5 * sizeof(float)), 1.0);
+
+        }
+    }
 
     return;
 
 }
 
+__global__ void ShiftPersonality(Agent* AgentField) {
+
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+
+    float shift = ((float) AgentField[j].PersonalityPressure / (float) AgentField[j].InfluenceNumber) / (float) AgentField[j].Ks;
+
+    AgentField[j].personality += shift;
+
+    return;
+}
+
 int main() {
 	// Image
 	const auto aspect_ratio = 1.0 / 1.0;
-	const int image_width = 400;
-    const int image_height = 400; // static_cast<int>(image_width / aspect_ratio);
-    float* personalityField = nullptr;
+	const int image_width = 100;
+    const int image_height = 100; // static_cast<int>(image_width / aspect_ratio);
+    Agent* AgentField = nullptr;
+    Agent* AgentField_debug = nullptr;
 
     // Image File
 
-    std::ofstream ImageFile_prev, ImageFile;
-    ImageFile_prev.open("Image_prev.ppm");
+    std::ofstream ImageFile_prev, ImageFile, ImageFile_diff;
+    ImageFile_prev.open("./images/0.ppm");
     ImageFile.open("Image.ppm");
+    ImageFile_diff.open("Image_diff.ppm");
 
     ImageFile_prev << "P3\n" << image_width << " " << image_height << "\n255\n";
     ImageFile << "P3\n" << image_width << " " << image_height << "\n255\n";
+    ImageFile_diff << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    personalityField = (float*) malloc(image_height * image_width * sizeof(float));
+    AgentField = (Agent*) malloc(image_height * image_width * sizeof(Agent));
+    AgentField_debug = (Agent*) malloc(image_height * image_width * sizeof(Agent));
 
 
     for (int j = 0; j < image_height; j++) {
@@ -48,40 +96,92 @@ int main() {
         for (int i = 0; i < image_width; i++) {
 
             float paranoia = random_double() * 2.0 - 1.0;
-            personalityField[j * image_width + i] = paranoia;
-            color PixelColor = mix(color(1.0, 0.0, 0.0), color(0.0, 0.0, 1.0), personalityField[j * image_width + i]);
+            float Kp = random_double() * 5.0;
+            float Reach = random_double() * 5.0;
+            float Ks = random_double() * 10.0;
+            AgentField[j * image_width + i].personality = paranoia;
+            AgentField[j * image_width + i].Kp = Kp;
+            AgentField[j * image_width + i].Reach = Reach;
+            AgentField[j * image_width + i].Ks = Ks;
+            AgentField[j * image_width + i].PersonalityPressure = 0.0;
+            AgentField[j * image_width + i].InfluenceNumber = 0.0;
+            AgentField_debug[j * image_width + i].personality = paranoia;
+            AgentField_debug[j * image_width + i].Kp = Kp;
+            AgentField_debug[j * image_width + i].Reach = Reach;
+            AgentField_debug[j * image_width + i].Ks = Ks;
+            AgentField_debug[j * image_width + i].PersonalityPressure = 0.0;
+            AgentField_debug[j * image_width + i].InfluenceNumber = 1.0;
+            color PixelColor = mix(color(1.0, 0.0, 0.0), color(0.0, 0.0, 1.0), AgentField[j * image_width + i].personality);
             write_color(ImageFile_prev, PixelColor);
 
         }
     }
 
-    float* KPersonalityField = NULL;
-    cudaMalloc(&KPersonalityField, image_height * image_width * sizeof(float));
+    Agent* KAgentField = NULL;
+    cudaMalloc(&KAgentField, image_height * image_width * sizeof(Agent));
 
-    cudaMemcpy(KPersonalityField, personalityField, image_height * image_width * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(KAgentField, AgentField, image_height * image_width * sizeof(Agent), cudaMemcpyHostToDevice);
 
-    simulate << <image_height, image_width >> > (KPersonalityField);
-    cudaDeviceSynchronize();
+    for (UINT t = 0; t < NUM_SIMULATIONS; t++) {
+        std::cout << "\rSimulation runs remaining: " << NUM_SIMULATIONS - t << ' ' << std::flush;
 
-    cudaMemcpy(personalityField, KPersonalityField, image_height * image_width * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(KAgentField, AgentField, image_height * image_width * sizeof(Agent), cudaMemcpyHostToDevice);
 
-    cudaFree(KPersonalityField);
+        ApplyPersonalityPressure << <image_height, image_width >> > (KAgentField, image_height, image_width);
+        cudaDeviceSynchronize();
+
+        ShiftPersonality << <image_height, image_width >> > (KAgentField);
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(AgentField, KAgentField, image_height * image_width * sizeof(Agent), cudaMemcpyDeviceToHost);
+
+        if (true) {
+            std::ofstream ImageFile_timestep;
+            ImageFile_timestep.open("./images/" + std::to_string(t + 1) + ".ppm");
+            ImageFile_timestep << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+            for (int j = image_height - 1; j >= 0; --j) {
+                //std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
+                for (int i = 0; i < image_width; ++i) {
+
+                    color PixelColor = mix(color(1.0, 0.0, 0.0), color(0.0, 0.0, 1.0), AgentField[j * image_width + i].personality);
+
+                    write_color(ImageFile_timestep, PixelColor);
+                }
+            }
+
+            //for (UINT i = 0; i < image_height * image_width; i++) {
+            //    Agent agent = AgentField[i];
+            //    continue;
+            //}
+
+            ImageFile_timestep.close();
+        }
+    }
+
+
+    cudaMemcpy(AgentField, KAgentField, image_height * image_width * sizeof(Agent), cudaMemcpyDeviceToHost);
+
+    cudaFree(KAgentField);
 
     for (int j = image_height - 1; j >= 0; --j) {
         //std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
 
-            color PixelColor = mix(color(1.0, 0.0, 0.0), color(0.0, 0.0, 1.0), personalityField[j * image_width + i]);
+            color PixelColor = mix(color(1.0, 0.0, 0.0), color(0.0, 0.0, 1.0), AgentField[j * image_width + i].personality);
 
             write_color(ImageFile, PixelColor);
 
+            PixelColor = mix(color(0.0, 0.0, 0.0), color(1.0, 1.0, 1.0), abs(AgentField_debug[j * image_width + i].personality - AgentField[j * image_width + i].personality));
+
+            write_color(ImageFile_diff, PixelColor);
         }
     }
 
     ImageFile_prev.close();
     ImageFile.close();
 
-    delete personalityField;
+    delete AgentField;
 
 	return 0;
 }
